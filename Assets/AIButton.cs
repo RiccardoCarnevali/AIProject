86021a2ab446aa8c4ed916d58ca39dd6873fd2e8
Matteo Puccio <matteopuccio2000@gmail.com>
2022-02-04 23:51:33 +0100

    Creation of Main Menu and Instruction Menu

---
+++ Assets/AIButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class AIButton : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(changeScene);
+    }
+    public void showImage(bool show)
+    {
+        Color tempColor = gameObject.GetComponent<Image>().color;
+        if (show)
+            tempColor.a = 1f;
+        else
+            tempColor.a = 0f;
+
+        gameObject.GetComponent<Image>().color = tempColor;
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
